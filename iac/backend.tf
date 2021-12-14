7ca8078c7a75c49cb669b4a5293f08d12d569059
dsanchor <dsanchoruiz@microsoft.com>
2021-12-14 09:18:13 +0100

    Change to own values

--- iac/backend.tf
+++ iac/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     # modify the values for the storage account
     storage_account_name = "sagaserverlesssa"
-    container_name       = "terraform"
+    container_name       = "testterraform"
     key                  = "terraform.tfstate"
   }
 }
